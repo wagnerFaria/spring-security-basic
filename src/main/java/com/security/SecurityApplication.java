919d237af27dd616b5cdbf875eb7eafaaf7ad678
Wagner Faria do Amaral <wagnerfaria1601@gmail.com>
2023-11-10 15:54:57 -0400

    Add initial users to application

--- src/main/java/com/security/SecurityApplication.java
+++ src/main/java/com/security/SecurityApplication.java
@@ -20,30 +20,30 @@ public class SecurityApplication {
 		SpringApplication.run(SecurityApplication.class, args);
 	}
 
-	@Bean
-	@Profile("!prod&&!test")
-	public CommandLineRunner commandLineRunner(
-			AuthenticationService service
-	) {
-		return args -> {
-			var admin = RegisterRequest.builder()
-					.firstname("Admin")
-					.lastname("Admin")
-					.email("admin@mail.com")
-					.password("password")
-					.role(ADMIN)
-					.build();
-			System.out.println("Admin token: " + service.register(admin).getAccessToken());
-
-			var manager = RegisterRequest.builder()
-					.firstname("Manager")
-					.lastname("Manager")
-					.email("manager@mail.com")
-					.password("password")
-					.role(MANAGER)
-					.build();
-			System.out.println("Manager token: " + service.register(manager).getAccessToken());
-
-		};
-	}
+//	@Bean
+//	@Profile("!prod&&!test")
+//	public CommandLineRunner commandLineRunner(
+//			AuthenticationService service
+//	) {
+//		return args -> {
+//			var admin = RegisterRequest.builder()
+//					.firstname("Admin")
+//					.lastname("Admin")
+//					.email("admin@mail.com")
+//					.password("admin")
+//					.role(ADMIN)
+//					.build();
+//			System.out.println("Admin token: " + service.register(admin).getAccessToken());
+//
+//			var manager = RegisterRequest.builder()
+//					.firstname("Manager")
+//					.lastname("Manager")
+//					.email("manager@mail.com")
+//					.password("manager")
+//					.role(MANAGER)
+//					.build();
+//			System.out.println("Manager token: " + service.register(manager).getAccessToken());
+//
+//		};
+//	}
 }
