f958b7dd7b2c13bf3f9b8139b57bc9e75a60b095
Wagner Faria do Amaral <wagnerfaria1601@gmail.com>
2023-11-11 07:08:49 -0400

    Tests for UserService running exceptions

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,5 @@ INSERT INTO "_USER" (email, firstname, lastname, password, role) VALUES ('admin-
 INSERT INTO "_USER" (email, firstname, lastname, password, role) VALUES ('manager-test@mail.com', 'Manager', 'Manager', '$2a$10$Yzwko0OK9DqfRZocI6rc1.ahpomze8V81ppSQ67scpzHq4SS/ol22', 'MANAGER');
 -- passwor: user
 INSERT INTO "_USER" (email, firstname, lastname, password, role) VALUES ('user-test@mail.com', 'User', 'User', '$2y$10$mm.Ocu3cnrPFQibK2F3dzeXqqZvi004wngI4FEM4IXA9oZ2qynymi', 'USER');
+-- user for change password test - passwor: user
+INSERT INTO "_USER" (email, firstname, lastname, password, role) VALUES ('user-changepassword-test@mail.com', 'User', 'User', '$2y$10$mm.Ocu3cnrPFQibK2F3dzeXqqZvi004wngI4FEM4IXA9oZ2qynymi', 'USER');
--- src/test/java/com/security/user/UserServiceTest.java
+++ src/test/java/com/security/user/UserServiceTest.java
@@ -1,44 +1,23 @@
 package com.security.user;
 
-import ch.qos.logback.core.CoreConstants;
 import com.security.config.ApplicationConfig;
-import com.security.utils.UserTestUtils;
-import org.junit.jupiter.api.BeforeEach;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
-import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.context.support.WithUserDetails;
-
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class UserServiceTest {
-
-    //    @MockBean
-//    private UserRepository userRepository;
     @Autowired
     private UserService userService;
     @Autowired
     private ApplicationConfig applicationConfig;
 
-    @BeforeEach
-    void setUp() {
-//        BDDMockito
-//                .when(userRepository.findByEmail(ArgumentMatchers.anyString()))
-//                .thenReturn(Optional.ofNullable(UserTestUtils.returnPersistedUser()));
-    }
-
     @Test
-    @WithUserDetails("user-test@mail.com")
+    @WithUserDetails("user-changepassword-test@mail.com")
     void changePassword() {
 
         Authentication authentication = SecurityContextHolder
@@ -53,5 +32,45 @@ class UserServiceTest {
                 .build();
 
         userService.changePassword(request, authentication);
+    }
+
+    @Test
+    @WithUserDetails("user-test@mail.com")
+    void changePasswordWithWrongPasswordShouldReturnIllegalStateException() {
+
+        Authentication authentication = SecurityContextHolder
+                .getContext()
+                .getAuthentication();
+
+        var request = ChangePasswordRequest
+                .builder()
+                .currentPassword("user1")
+                .newPassword("1234567")
+                .confirmationPassword("1234567")
+                .build();
+
+        Assertions
+                .assertThatThrownBy(() -> userService.changePassword(request, authentication))
+                .isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    @WithUserDetails("user-test@mail.com")
+    void changePasswordWithWrongConfirmationPasswordShouldReturnIllegalStateException() {
+
+        Authentication authentication = SecurityContextHolder
+                .getContext()
+                .getAuthentication();
+
+        var request = ChangePasswordRequest
+                .builder()
+                .currentPassword("user")
+                .newPassword("1234567")
+                .confirmationPassword("12345678")
+                .build();
+
+        Assertions
+                .assertThatThrownBy(() -> userService.changePassword(request, authentication))
+                .isInstanceOf(IllegalStateException.class);
     }
 }
